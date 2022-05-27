628d74422d8879a3c36f172ad579e5abcc658e90
prmacia <pablo-macia@live.com.ar>
2022-05-26 21:56:05 -0300

    home a medias

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+let mainImg = document.getElementById("main-img");
+console.log(mainImg.clientHeight)
