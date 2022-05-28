92871397b2ab9f053e98d47bb6219b4cac464daa
prmacia <pablo-macia@live.com.ar>
2022-05-28 15:39:25 -0300

    Producción

--- js/main.js
+++ js/main.js
@@ -3,5 +3,24 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
 
-let mainImg = document.getElementById("main-img");
-console.log(mainImg.clientHeight)
+let img = document.querySelector("#main-img img");
+let box1 = document.querySelector("#box1");
+let box2 = document.querySelector("#box2");
+let box3 = document.querySelector("#box3");
+
+
+box1.addEventListener("mouseover",function(e){
+  console.log(img.src)
+  img.src="assets/img/box1.gif"
+});
+
+box2.addEventListener("mouseover",function(e){
+  console.log(img.src)
+  img.src="assets/img/box2.png"
+});
+
+box3.addEventListener("mouseover",function(e){
+  console.log(img.src)
+  img.src="assets/img/box3.png"
+});
+
